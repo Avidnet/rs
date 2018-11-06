5e032c3ce248f3d889c14dde8ca2dadb5b58ac4e
Parham Alvani <parham.alvani@gmail.com>
2018-11-06 20:43:01 +0330

    Calls more methods on GeoTIFF.

--- index.js
+++ index.js
@@ -13,11 +13,25 @@
 const fs = require('fs')
 const GeoTIFF = require('geotiff')
 
-
+// readTIFF reads a tiff image from a default location. This function is just for testing
+// the basic features of geotiff.js.
 const readTIFF = async () => {
   const arrayBuffer = fs.readFileSync('./sample/joveyn.tif', null).buffer
   const tiff = await GeoTIFF.fromArrayBuffer(arrayBuffer)
   const image = await tiff.getImage() // by default, the first image is read.
-  console.log(image)
+
+  const tileWidth = image.getTileWidth()
+  const tileHeight = image.getTileHeight()
+  console.log(tileWidth)
+  console.log(tileHeight)
+
+  // the number of samples per pixel
+  console.log(image.getSamplesPerPixel())
+  const origin = image.getOrigin()
+  const resolution = image.getResolution()
+  const bbox = image.getBoundingBox()
+  console.log(origin)
+  console.log(resolution)
+  console.log(bbox)
 }
 readTIFF()
