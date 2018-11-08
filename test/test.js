ae469e5fa77b84f54b6fd892104ded58f56c1d7b
Parham Alvani <parham.alvani@gmail.com>
2018-11-08 17:59:41 +0330

    Tests basic features of storage.

--- test/test.js
+++ test/test.js
@@ -15,16 +15,18 @@
 const assert = require('assert')
 const fs = require('fs')
 const GeoTIFF = require('geotiff')
+const Storage = require('../lib/storage')
 
-const lat = 36.65449
-const lng = 57.41716
-const result = 4.337339878082275
+require('dotenv').config()
 
 describe('Joveyn', function () {
   // readTIFF reads a tiff image from a given path then parse its meta data.
   // This function is just for testing
   // the basic features of geotiff.js.
   // We use joveyn as our refrence point.
+  const lat = 36.65449
+  const lng = 57.41716
+  const result = 4.337339878082275
   const path = './sample/joveyn.tif'
 
   it('Neghab Railway Station', async () => {
@@ -57,5 +59,21 @@ describe('Joveyn', function () {
         }
       }
     }
+  })
+})
+
+describe('Storage', function () {
+  it('I1820', async () => {
+    const value = 1.33
+    const lat = 35.8066282
+    const lng = 51.3989276
+    const date = '07-Sep-2016'
+
+    const storage = new Storage()
+    await storage.connect()
+    await storage.insert(value, lat, lng, date)
+    const res = await storage.fetch(35.807715, 51.398652)
+    console.table(res)
+    await storage.disconnect()
   })
 })
